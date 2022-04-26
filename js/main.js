57edfcef1071cff494ce9b26144a71ec838d73d7
Ubuntu <ubuntu@ip-172-31-33-162.ap-northeast-1.compute.internal>
2022-04-26 08:24:19 +0000

    homepage

--- js/main.js
+++ js/main.js
@@ -247,3 +247,19 @@ fetch(rabbit_url)
     console.error(`エラーが発生しました： ${error.message}`);
   });
   
+/**
+ * -----------------------------------------------------------------------------
+ * メニューからクリック後にスクロール遷移
+ * -----------------------------------------------------------------------------
+ **/
+ $(function(){
+  $('a[href^="#"]').click(function(){
+    var adjust = 0;
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
